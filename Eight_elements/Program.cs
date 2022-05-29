1ae2697f0bd0bfb1e0778e190bc58bdc5e5bfa69
Mira <5555159m@gmail.com>
2022-05-29 21:37:55 +0300

    add 1 task

--- Eight_elements/Program.cs
+++ Eight_elements/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+Console.Clear();
+int [] Binare = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    Binare[i] = new Random().Next(0,2);
+    Console.Write(Binare[i] + " ");
+}
