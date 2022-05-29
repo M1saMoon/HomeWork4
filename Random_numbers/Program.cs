efd418a785f0f454b10677c1b03bfef27d2ae46e
Mira <5555159m@gmail.com>
2022-05-29 23:10:11 +0300

    add 3 task

--- Random_numbers/Program.cs
+++ Random_numbers/Program.cs
@@ -1,3 +1,22 @@
 ﻿//Задать массив, заполнить случайными положительными трёхзначными числами. 
 //Показать количество нечетных\четных чисел
 
+Console.Clear();
+int [] array = new int[15];
+int countEven = 0;
+int countOdd = 0;
+for ( int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write(array[i] + " ");
+
+    if (array[i] % 2 == 0)
+    countEven++;
+    else 
+    countOdd++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел: {countEven}");
+Console.WriteLine($"Количество нечетных чисел: {countOdd}");
+
+
