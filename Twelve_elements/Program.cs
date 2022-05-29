cd86ba7108b5e31fd89a5a36fd3a801b47441503
Mira <5555159m@gmail.com>
2022-05-29 22:50:56 +0300

    add 2 task

---
+++ Twelve_elements/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9].
+// Найти сумму положительных/отрицательных элементов массива
+
+Console.Clear();
+int [] array = new int[12];
+int sumPos = 0;
+int sumNeg = 0;
+for ( int i = 0; i < 12; i++)
+{
+   array[i] = new Random().Next(0,10);
+   Console.Write(array[i] + " ");
+
+   if ( array [i] >= 0)
+   sumPos = sumPos + array [i];
+   else 
+   Console.WriteLine(sumNeg);
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных элементов: {sumPos}");
+Console.WriteLine($"Сумма отрицательных элементов: {sumNeg}");
+
+
+
+
