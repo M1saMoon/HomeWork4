2085bbb70cf85f0c26103071ac6a5fea51405f71
Mira <5555159m@gmail.com>
2022-05-29 23:26:40 +0300

    add 4 task

--- Line_segment/Program.cs
+++ Line_segment/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+Console.Clear();
+int[] array = new int[123];
+int count = 0;
+for ( int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-1000,1000);
+    Console.Write(array[i] + " ");
+    if (array[i] > 10 && array[i] < 99)
+    count++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество элементов о отрезке: {count}");
