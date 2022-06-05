133840692c34ae5ae02a047b3319504dccbdcc2c
Mira <5555159m@gmail.com>
2022-06-05 18:29:08 +0300

    add 5 task

--- Pair_product/Program.cs
+++ Pair_product/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найти произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Console.Clear();
+int [] array = new int [20];
+int count = 0;
+for ( int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,20);
+    Console.Write($"{array[i] + " "}");
+}
+Console.WriteLine();
+for (int i = 0; i < 10; i++)
+{
+    count = array[i] * array.Length - 1;
+}
+Console.WriteLine($"{count}");
